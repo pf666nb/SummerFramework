507833ec1b54f3ad1034f8edfdbc472d7b0e2157
pf666nb <1340701723@qq.com>
2023-07-25 15:02:37 +0800

    初始化SummerFramework的文件读取部分

---
+++ src/main/java/org/SummerFramework/summer/io/Resource.java
@@ -0,0 +1,12 @@
+package org.SummerFramework.summer.io;
+
+/**
+ * SummerFramework
+ * 资源文件
+ *
+ * @author : wpf
+ * @date : 2023-07-25 14:03
+ **/
+public record Resource(String path, String name) {
+
+}
